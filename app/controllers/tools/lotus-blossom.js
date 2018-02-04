e945efe23fcfb4dff39b5e67137c083eb7e7e97a
Evan Martinez <evanm.design@gmail.com>
2018-02-04 08:47:17 -0800

    first typeform

--- app/controllers/tools/lotus-blossom.js
+++ app/controllers/tools/lotus-blossom.js
@@ -3,10 +3,14 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	aiOpen: false,
 	modalOpen: true,
+	step: 1,
 
 	actions: {
 		closeModal() {
 			this.set('modalOpen', false);
+		},
+		submitLotusCenter() {
+			console.log('done');
 		}
 	}
 });
